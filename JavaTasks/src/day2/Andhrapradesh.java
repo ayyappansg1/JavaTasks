a324ed62ad6b598222d99d3b2c0bb2002b31f76d
ayyappansg1 <104506082+ayyappansg1@users.noreply.github.com>
2022-05-10 19:52:13 +0530

    Update Andhrapradesh.java

--- JavaTasks/src/day2/Andhrapradesh.java
+++ JavaTasks/src/day2/Andhrapradesh.java
@@ -1,10 +1,5 @@
 public class Andhrapradesh {
-	
-	public void Telugu() {
-		System.out.println("Telugu language offen speaken in Andhra");
-	}
 	public static void main(String[]args){
-		Andhrapradesh an=new Andhrapradesh();
-		an.Telugu();
+		System.out.println("Sangar dude is andhra");
 	}
 }
